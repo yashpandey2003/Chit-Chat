e9a8a14810917496b32e6c2a811d5d634ff28591
yashpandey2003 <yashisro@gmail.com>
2024-01-30 19:56:16 +0530

    Added chat functionality

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,35 +1,37 @@
 import React from 'react'
 import styled from 'styled-components';
 import Logout from './Logout';
+import ChatInput from './ChatInput';
+import Messages from './Messages';
 
 const ChatContainer = ({ currentChat }) => {
-    return (
-        <>
-            {
-                currentChat && (
+  const handleSendMsg = async (msg) => {
+    
+  }
+  return (
+    <>
+      {
+        currentChat && (
 
-                    <Container>
-                        <div className='chat-header'>
-                            <div className='user-details'>
-                                <div className='avatar'>
-                                    <img src={`data:image/svg+xml;base64,${currentChat.avatarImage}`} alt="avatar" /></div>
-                                    <div className='username'>
-                                        <h3>{currentChat.username}</h3>
-                                    </div>
-                                    </div>
-                                    <Logout />
-                                </div>
-                        <div className='chat-messages'>
-                            <div className='chat-input'>
+          <Container>
+            <div className='chat-header'>
+              <div className='user-details'>
+                <div className='avatar'>
+                  <img src={`data:image/svg+xml;base64,${currentChat.avatarImage}`} alt="avatar" /></div>
+                <div className='username'>
+                  <h3>{currentChat.username}</h3>
+                </div>
+              </div>
+              <Logout />
+            </div>
+            <Messages />
+            <ChatInput handleSendMsg={handleSendMsg} />
+          </Container>
 
-                            </div>
-                        </div>
-                    </Container>
-
-                )
-            }
-        </>
-    )
+        )
+      }
+    </>
+  )
 }
 
 const Container = styled.div`
