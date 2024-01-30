9da96f00a4975312211e1c1713900a075cb09546
yashpandey2003 <yashisro@gmail.com>
2024-01-31 02:02:48 +0530

    Socket implemented, project completed

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import { v4 as uuidv4 } from "uuid";
 import styled from 'styled-components';
 import Logout from './Logout';
 import ChatInput from './ChatInput';
-import Messages from './Messages';
 import axios from 'axios';
 import { getAllMessagesRoute, sendMessageRoute } from '../utils/APIRoutes';
 
-const ChatContainer = ({ currentChat, currentUser }) => {
+
+const ChatContainer = ({ currentChat, currentUser, socket }) => {
   const [messages, setMessages] = useState([]);
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+  const scrollRef = useRef();
 
+  useEffect(() => {
+    async function func() {
+      if (currentChat) {
+        const response = await axios.post(getAllMessagesRoute, {
+          from: currentUser._id,
+          to: currentChat._id,
 
-
-  useEffect(()=>{
-    async function func(){
-      const response = await axios.post(getAllMessagesRoute, {
-        from:currentUser._id,
-        to: currentChat._id,
-
-      })
-      setMessages(response.data);
+        });
+        setMessages(response.data);
+      }
     };
     func();
   }, [currentChat])
@@ -27,12 +30,45 @@ const ChatContainer = ({ currentChat, currentUser }) => {
 
 
   const handleSendMsg = async (msg) => {
+    const data = await JSON.parse(
+      localStorage.getItem("chat-app-user")
+    );
+    socket.current.emit("send-msg", {
+      to: currentChat._id,
+      from: data._id,
+      msg,
+    });
+
     await axios.post(sendMessageRoute, {
-      from:currentUser._id,
+      from: currentUser._id,
       to: currentChat._id,
       message: msg,
-    })
-  }
+    });
+    const msgs = [...messages];
+    msgs.push({ fromSelf: true, message: msg });
+    setMessages(msgs);
+  };
+
+  useEffect(() => {
+    if (socket.current) {
+      socket.current.on("msg-receive", (msg) => {
+        setArrivalMessage({ fromSelf: false, message: msg });
+      });
+    }
+
+  }, []);
+
+  useEffect(() => {
+    arrivalMessage && setMessages((prev) => {
+      return [...prev, arrivalMessage];
+    });
+  }, [arrivalMessage]);
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+
   return (
     <>
       {
@@ -50,21 +86,20 @@ const ChatContainer = ({ currentChat, currentUser }) => {
               <Logout />
             </div>
             <div className='chat-messages'>
-              {
-                messages.map((message) => {
-                  return (
-                    <div>
-                    <div className={`message ${message.fromSelf ? "sended" : "received" }`}>
-                      <div className='content'>
-                        <p>
-                          {message.message}
-                        </p>
+              {messages?.map((message) => {
+                return (
+                  <div ref={scrollRef} key={uuidv4()}>
+                    <div
+                      className={`message ${message.fromSelf ? "sended" : "received"
+                        }`}
+                    >
+                      <div className="content ">
+                        <p>{message.message}</p>
                       </div>
                     </div>
-                    </div>
-                  )
-                })
-              }
+                  </div>
+                );
+              })}
             </div>
             <ChatInput handleSendMsg={handleSendMsg} />
           </Container>
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,13 +1,15 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import styled from 'styled-components';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
-import { allUsersRoute } from '../utils/APIRoutes';
+import { allUsersRoute, host } from '../utils/APIRoutes';
 import Contacts from '../components/Contacts';
 import Welcome from '../components/Welcome';
 import ChatContainer from '../components/ChatContainer';
+import {io} from "socket.io-client";
 
 const Chat = () => {
+  const socket = useRef();
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
@@ -26,6 +28,14 @@ const Chat = () => {
     
     check();
   }, [])
+  useEffect(()=>{
+    if(currentUser){
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+
+    }
+
+  }, [currentUser])
   useEffect(()=>{
     async function contactFetch(){
       if(currentUser){
@@ -48,7 +58,7 @@ const Chat = () => {
       <div className='container'>
       <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
       {
-        isLoaded && currentChat===undefined ? (<Welcome currentUser={currentUser} />) : (<ChatContainer currentChat = {currentChat} currentUser={currentUser} />)
+        isLoaded && currentChat===undefined ? (<Welcome currentUser={currentUser} />) : (<ChatContainer currentChat = {currentChat} currentUser={currentUser} socket={socket} />)
       }
       
       </div>
