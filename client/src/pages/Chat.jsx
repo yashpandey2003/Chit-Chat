59af9d50c90913a6dbb165d8f52c3096e03f3edd
yashpandey2003 <yashisro@gmail.com>
2024-01-30 18:52:31 +0530

    Created chat container and logout functionality

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -4,12 +4,15 @@ import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 import { allUsersRoute } from '../utils/APIRoutes';
 import Contacts from '../components/Contacts';
+import Welcome from '../components/Welcome';
+import ChatContainer from '../components/ChatContainer';
 
 const Chat = () => {
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [currentChat, setCurrentChat] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
   useEffect(()=>{
     async function check(){
       if (!localStorage.getItem("chat-app-user"))
@@ -17,6 +20,7 @@ const Chat = () => {
 
       else{
         setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+        setIsLoaded(true);
       }
     }
     
@@ -43,6 +47,10 @@ const Chat = () => {
     <Container>
       <div className='container'>
       <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+      {
+        isLoaded && currentChat===undefined ? (<Welcome currentUser={currentUser} />) : (<ChatContainer currentChat = {currentChat} />)
+      }
+      
       </div>
     </Container>
   )
