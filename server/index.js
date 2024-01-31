00ccb55dfe8a991c2f64938020c3b5ca1b17b844
yashpandey2003 <yashisro@gmail.com>
2024-01-31 11:35:17 +0530

    Final commit

--- server/index.js
+++ server/index.js
@@ -8,11 +8,29 @@ const http = require('http');
 const userRoutes = require('./routes/userRoutes');
 const messageRoute = require('./routes/messagesRoute');
 require("dotenv").config();
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoute);
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV==='production'){
+
+  app.use(express.static(path.join(__dirname1, '../client/dist')));
+
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname1, "..", "client", "dist", "index.html"));
+  });
+
+}
+else{
+  app.get("/", (req, res)=>{
+    res.send("API is running successfully.")
+  })
+}
+
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
@@ -22,6 +40,9 @@ connectDb().then(() => {
         console.log(`Server is running on port ${PORT}`);
     });
 });
+
+
+
 
 const io = socket(server, {
     cors: {
